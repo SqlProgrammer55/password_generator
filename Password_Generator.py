55583cf9f141082b1920e1c450db8b8ca2d93d1f
SqlProgrammer55 <80296566+SqlProgrammer55@users.noreply.github.com>
2024-02-20 09:19:06 -0800

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -6,11 +6,12 @@ PasswordLength = input("Input Desired Password Length (Minimum 10 Characters): "
     #Line 16 or below runs first, then function. 
 # "length" is an arbitrary character, contains no values, it is blank intially. 
 
-def generate_password(length):
-    password = secrets.token_hex(int(PasswordLength)) # Generate a secure random string of alpha numeric characters
-                                                      #  the "PasswordLength" value immediatley fills in all "length" with the user input.
-    password = secrets.token_hex(length // 2)         # rounds down to the nearest whole number
-        # Return the first `length` characters of the password
+def generate_password(length):                  # the "PasswordLength" value immediatley fills in all "length" with the user input.
+                                                    #Passes down the value to the "length" line below.
+    password = secrets.token_hex(length // 2)   # Generates a secure random string of alpha numeric characters
+                                                # rounds down to the nearest whole number
+    #Deprecated, not needed: password = secrets.token_hex(int(PasswordLength)) 
+        # Return, returns the first `length` characters of the password generated above. 
     return password 
 
 randompassword = generate_password(int(PasswordLength)) #int removes decimals if any, and converts to int, to pass to "length". 
